9cce66a5fcb83c1443356b614fd0f2d98ce20009
MathiosBR <145026746+MathiosBR@users.noreply.github.com>
2024-10-11 20:06:24 -0300

    Update and rename SistemaFotovoltaico.java to SistemaEmpresa.java
    Ajustes iniciais do dimensionamento para solicitação de projeto afim de, ao iniciar a submissão do consumo médio do cliente e da irradiação da sua região, poder realizar a solicitação com base nisso.

---
+++ ProjetoU2POO/ProjetoU2POO/src/empresa/SistemaEmpresa.java
@@ -0,0 +1,59 @@
+package empresa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cliente.Cliente;
+
+public class SistemaEmpresa {
+	private int Npaineis, Ninversores;
+	private Cliente cliente;
+	private List<Projeto> projetos;
+	
+	public SistemaEmpresa() {
+		this.projetos = new ArrayList<>();
+	}
+	
+	public void setNpaineis(int npaineis) {
+		Npaineis = npaineis;
+	}
+
+	public void setNinversores(int ninversores) {
+		Ninversores = ninversores;
+	}
+
+	public int getNpaineis() {
+		return Npaineis;
+	}
+	public int getNinversores() {
+		return Ninversores;
+	}
+
+	//Métodos
+	
+	public void cadastrarCliente(String nome, String endereco, String telefone) {
+		this.cliente.setNome(nome);
+		this.cliente.setEndereco(endereco);
+		this.cliente.setTelefone(telefone);
+	}
+	
+	//Nesse método, o funcionário pode passar o valor da potencia da placa e do inversor diretamente
+	//ou
+	//através de placa e inversor que o cliente deseja adquirir, dados preços.
+	//Além disso, solicita um novo projeto baseado no
+	public void solicitarProjeto(Cliente cliente, int consumoMensal, double potenciaPlaca, double potenciaInversor, double irradiacao) {
+		 
+		//Cálculo do Dimensionamento do SistemaFotovoltaico
+		setNpaineis((int)Math.ceil((consumoMensal)/(irradiacao*potenciaPlaca*30)));
+		setNinversores((int)Math.ceil((getNpaineis()*potenciaPlaca)/potenciaInversor));
+		//Fim do Cálculo
+		
+		System.out.println("Número de paineis necessários: " + getNpaineis());
+		System.out.println("Número de inversores necessários: " + getNinversores());
+		
+		Projeto projeto = new Projeto(cliente, consumoMensal);
+		if(!projetos.contains(projeto)) {
+			projetos.add(projeto);
+		}
+	}
+}
