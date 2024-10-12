5ae646602b9dcb37cd1b3ed26b50cfb9e1b55182
luan-feitosa <146037049+luan-feitosa@users.noreply.github.com>
2024-10-12 13:08:21 -0300

    Update Fabricante.java
    Erro consertado

--- ProjetoU2POO/ProjetoU2POO/src/empresa/Fabricante.java
+++ ProjetoU2POO/ProjetoU2POO/src/empresa/Fabricante.java
@@ -1,37 +1,33 @@
-package cliente;
+package empresa;
 
-public class Projeto {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fabricante {
 	private String nome;
-	private Cliente cliente;
-	private int consumoMedio;
+	private List<PlacaSolar> placasSolares;
+	private List<Inversor> inversores;
 	
-	public Projeto(String nome, Cliente cliente, int consumoMedio) {
+	public Fabricante(String nome){
 		this.nome = nome;
-		this.cliente = cliente;
-		this.consumoMedio = consumoMedio;
+		this.placasSolares = new ArrayList<>();
+		this.inversores = new ArrayList<>();
 	}
 
+	//Getter e Setter
 	public String getNome() {
 		return nome;
-	} 
-	
-	public Cliente getCliente() {
-		return cliente;
 	}
 
-	public int getConsumoMedio() {
-		return consumoMedio;
-	}
-	
-	public void setNome (String nome) {
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
+	
+	public void adicionarPlacasSolares(PlacaSolar placaSolar) {
+		placasSolares.add(placaSolar);
 	}
-
-	public void setConsumoMedio(int consumoMedio) {
-		this.consumoMedio = consumoMedio;
+	public void adicionarInversores(Inversor inversor) {
+		inversores.add(inversor);
 	}
+	
 }
