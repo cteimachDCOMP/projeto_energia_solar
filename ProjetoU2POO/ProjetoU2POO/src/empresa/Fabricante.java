86e79c20b20f429c20b027621c40b31c74fbfe40
MathiosBR <145026746+MathiosBR@users.noreply.github.com>
2024-10-13 11:51:08 -0300

    Update Fabricante.java

--- ProjetoU2POO/ProjetoU2POO/src/empresa/Fabricante.java
+++ ProjetoU2POO/ProjetoU2POO/src/empresa/Fabricante.java
@@ -1,17 +1,10 @@
 package empresa;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Fabricante {
 	private String nome;
-	private List<PlacaSolar> placasSolares;
-	private List<Inversor> inversores;
 	
 	public Fabricante(String nome){
 		this.nome = nome;
-		this.placasSolares = new ArrayList<>();
-		this.inversores = new ArrayList<>();
 	}
 
 	//Getter e Setter
@@ -21,13 +14,6 @@ public class Fabricante {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-	
-	public void adicionarPlacasSolares(PlacaSolar placaSolar) {
-		placasSolares.add(placaSolar);
-	}
-	public void adicionarInversores(Inversor inversor) {
-		inversores.add(inversor);
 	}
 	
 }
