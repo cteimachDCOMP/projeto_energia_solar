5d9b7696f54385eece98e48275eba5151330b1cf
luan-feitosa <146037049+luan-feitosa@users.noreply.github.com>
2024-10-13 12:56:39 -0300

    Update Main.java
    Adicionadas funcionalidades dos botões Cadastrar Fabricante, Cadastrar Placa Solar e Cadastrar Inversor, além de atualizadas funções de Cadastrar Cliente e Projeto

--- ProjetoU2POO/src/main/Main.java
+++ ProjetoU2POO/src/main/Main.java
@@ -3,8 +3,10 @@ package main;
 import javax.swing.*;
 
 import cliente.Cliente;
-import empresa.Projeto;
-import empresa.SistemaEmpresa;
+import cliente.Projeto;
+import empresa.Fabricante;
+import empresa.Inversor;
+import empresa.PlacaSolar;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -17,10 +19,11 @@ public class Main {
 
     public static void main(String[] args) {
         // Cria lista de clientes e projetos
-    	SistemaEmpresa sistema = new SistemaEmpresa();    	
-    	
         ArrayList<Cliente> clientes = new ArrayList<>();
         ArrayList<Projeto> projetos = new ArrayList<>();
+        ArrayList<Fabricante> fabricantes = new ArrayList<>();
+        ArrayList<PlacaSolar> placasSolares = new ArrayList<>();
+        ArrayList<Inversor> inversores = new ArrayList<>();
 
         // Cria o frameInicial
         JFrame frameInicial = new JFrame("Você é...?");
@@ -44,6 +47,7 @@ public class Main {
         JButton buttonCadastrarCliente = new JButton("Cadastrar Cliente");
         JButton buttonCadastrarProjeto = new JButton("Cadastrar Projeto");
         JButton buttonListarProjetos = new JButton("Listar Projetos");
+        JButton buttonRetornarDeClienteParaInicial = new JButton("Retornar");
         
         // Cria o frameFuncionario
         JFrame frameFuncionario = new JFrame("Bem-vindo, funcionário! Escolha a operação a ser executada");
@@ -92,22 +96,71 @@ public class Main {
         
         JLabel labelConsumoMedio = new JLabel("Insira o seu consumo médio de energia");
         JTextField textFieldConsumoMedio = new JTextField();
-        
-        JLabel labelIrradiacao = new JLabel("Insira a irradiação da sua região (em kWh/m².dia): ");
-        JTextField textFieldIrradiacao = new JTextField();
 
         JButton buttonSubmeterProjeto = new JButton("Submeter");
         
+        // Cria o frameInputFabricante
+        JFrame frameInputFabricante = new JFrame("Cadastrar Fabricante");
+        frameInputFabricante.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Changed from EXIT_ON_CLOSE
+        frameInputFabricante.setSize(400, 300);
         
+        JPanel panelInputFabricante = new JPanel();
+        panelInputFabricante.setLayout(new GridLayout(5, 2, 10, 10));
         
+        JLabel labelNomeDoFabricante = new JLabel("Insira o nome do fabricante: ");
+        JTextField textFieldNomeDoFabricante = new JTextField();
         
         JButton buttonSubmeterFabricante = new JButton("Submeter");
+        
+        // Cria o frameInputPlacaSolar
+        JFrame frameInputPlacaSolar = new JFrame("Cadastrar Placa Solar");
+        frameInputPlacaSolar.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Changed from EXIT_ON_CLOSE
+        frameInputPlacaSolar.setSize(400, 300);
+        
+        JPanel panelInputPlacaSolar = new JPanel();
+        panelInputPlacaSolar.setLayout(new GridLayout(5, 2, 10, 10));
+        
+        JLabel labelNomeDaPlacaSolar = new JLabel("Insira o nome do placa solar: ");
+        JTextField textFieldNomeDaPlacaSolar = new JTextField();
+        
+        JLabel labelCapacidade = new JLabel("Insira a capacidade da placa solar: ");
+        JTextField textFieldCapacidade = new JTextField();
+        
+        JLabel labelPreçoDaPlacaSolar = new JLabel("Insira o preço da placa solar: ");
+        JTextField textFieldPreçoDaPlacaSolar = new JTextField();
+        
+        JLabel labelFabricantes = new JLabel("Insira os fabricantes da placa solar: ");
+        JTextField textFieldFabricantesDaPlacaSolar = new JTextField();
+        
+        JButton buttonSubmeterPlacaSolar = new JButton("Submeter");
+        
+        // Cria o frameInputInversor 
+        JFrame frameInputInversor = new JFrame("Cadastrar Inversor");
+        frameInputInversor.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frameInputInversor.setSize(400, 300);
+        
+        JPanel panelInputInversor = new JPanel();
+        panelInputInversor.setLayout(new GridLayout(5, 2, 10, 10));
+        
+        JLabel labelModeloDoInversor = new JLabel("Insira o modelo do inversor: ");
+        JTextField textFieldModeloDoInversor = new JTextField();
+        
+        JLabel labelPotencia = new JLabel("Insira a potência do inversor: ");
+        JTextField textFieldPotencia = new JTextField();
+        
+        JLabel labelPreçoDoInversor = new JLabel("Insira o preço do inversor: ");
+        JTextField textFieldPreçoDoInversor = new JTextField();
+        
+        JLabel labelFabricanteDoInversor = new JLabel("Insira o fabricante do inversor: ");
+        JTextField textFieldFabricanteDoInversor = new JTextField();
+        
+        JButton buttonSubmeterInversor = new JButton("Submeter");
 
         // Adiciona um action listener para cada botão
         buttonCliente.addActionListener(e -> {
-                frameInicial.setVisible(false);  // Hide frameInicial
-                frameCliente.setLocationRelativeTo(null);
-                frameCliente.setVisible(true);
+        	frameInicial.setVisible(false);  // Hide frameInicial
+        	frameCliente.setLocationRelativeTo(null);
+        	frameCliente.setVisible(true);
         });
 
         buttonFuncionario.addActionListener(e -> {
@@ -134,15 +187,21 @@ public class Main {
         });
         
         buttonCadastrarFabricante.addActionListener(e -> {
-        	
+        	frameFuncionario.setVisible(false);
+        	frameInputFabricante.setLocationRelativeTo(null);
+        	frameInputFabricante.setVisible(true);
         });
         
         buttonCadastrarPlacaSolar.addActionListener(e -> {
-        	
+        	frameFuncionario.setVisible(false);
+        	frameInputPlacaSolar.setLocationRelativeTo(null);
+        	frameInputPlacaSolar.setVisible(true);
         });
 
         buttonCadastrarInversor.addActionListener(e -> {
-        	
+        	frameFuncionario.setVisible(false);
+        	frameInputInversor.setLocationRelativeTo(null);
+        	frameInputInversor.setVisible(true);
         });
         
         buttonSubmeterCliente.addActionListener(e -> {
@@ -151,14 +210,13 @@ public class Main {
                 String endereço = textFieldEndereço.getText();
                 String telefone = textFieldTelefone.getText();
 
-                sistema.cadastrarCliente(nome, endereço, telefone);  // Cadastrar cliente
+                clientes.add(new Cliente(nome, endereço, telefone));  // Criar cliente
 
                 for (Cliente cliente : clientes) {
                 	if (cliente.getNome().equals(nome)) {
                 		JOptionPane.showMessageDialog(frameInputCliente, "Cliente com os seguintes dados:/nNome: " + cliente.getNome() + "/nEndereço: " + cliente.getEndereco() + "/nTelefone: " + cliente.getTelefone() + "/ncadastrado com sucesso!");
                 	}
                 }
-                
                 // Opcional: Limpar campos de texto após o cadastro
                 textFieldNomeDoCliente.setText("");
                 textFieldEndereço.setText("");
@@ -169,27 +227,84 @@ public class Main {
         	String nomeDoRequisitador = textFieldNomeDoRequisitador.getText();
         	String nomeDoProjeto = textFieldNomeDoProjeto.getText();
         	String consumoMedio = textFieldConsumoMedio.getText();
-        	String irradiacao = textFieldIrradiacao.getText();
-        	
         	
         	int intConsumoMedio = Integer.valueOf(consumoMedio);
-        	double doubleIrradiacao = Float.valueOf(irradiacao);
         	
         	for (Cliente cliente : clientes) {
         		if (cliente.getNome().equals(nomeDoRequisitador)) {
-        			sistema.dimensionarSistema(cliente, nomeDoProjeto, intConsumoMedio, doubleIrradiacao, null, null);
+        			projetos.add(new Projeto(nomeDoProjeto, cliente, intConsumoMedio));
         		}
         	}
         	
         	for (Projeto projeto : projetos) {
-        		if (projeto.getNomeProjeto().equals(nomeDoProjeto)) {
-        			JOptionPane.showMessageDialog(frameInputProjeto, "Projeto com os seguintes dados:/nNome:" + projeto.getNomeProjeto() + "/nCliente: " + projeto.getCliente() + "/nConsumo Médio: " + projeto.getConsumoMensal());
+        		if (projeto.getNome().equals(nomeDoProjeto)) {
+        			JOptionPane.showMessageDialog(frameInputProjeto, "Projeto com os seguintes dados:/nNome:" + projeto.getNome() + "/nCliente: " + projeto.getCliente() + "/nConsumo Médio: " + projeto.getConsumoMedio() + "/nFoi cadastrado com sucesso!");
         		}
         	}
         });
         
         buttonSubmeterFabricante.addActionListener(e -> {
+        	String nomeDoFabricante = textFieldNomeDoFabricante.getText();
         	
+        	fabricantes.add(new Fabricante(nomeDoFabricante));
+        	
+        	for (Fabricante fabricante : fabricantes) {
+        		if (fabricante.getNome().equals(nomeDoFabricante)) {
+        			JOptionPane.showMessageDialog(frameInputFabricante, "Projeto com o seguinte nome:/n" + fabricante.getNome() + "/nFoi cadastrado com sucesso!");
+        		}
+        	}
+        });
+        
+        buttonSubmeterPlacaSolar.addActionListener(e -> {
+        	String nomeDaPlacaSolar = textFieldNomeDaPlacaSolar.getText();
+        	String capacidade = textFieldCapacidade.getText();
+        	String preçoDaPlacaSolar = textFieldPreçoDaPlacaSolar.getText();
+        	String fabricanteDaPlacaSolar = textFieldFabricantesDaPlacaSolar.getText();
+        	
+        	double doubleCapacidade = Double.valueOf(capacidade);
+        	double doublePreçoDaPlacaSolar = Double.valueOf(preçoDaPlacaSolar);
+        	
+        	Fabricante objFabricanteDaPlacaSolar = null;
+        	
+        	for (Fabricante fabricante : fabricantes) {
+        		if (fabricante.getNome().equals(fabricanteDaPlacaSolar)) {
+        			objFabricanteDaPlacaSolar = fabricante;
+        		}
+        	}
+        	
+        	placasSolares.add(new PlacaSolar(nomeDaPlacaSolar, doubleCapacidade, doublePreçoDaPlacaSolar, objFabricanteDaPlacaSolar));
+        	
+        	for (PlacaSolar placaSolar : placasSolares) {
+        		if (placaSolar.getModelo().equals(nomeDaPlacaSolar)) {
+        			JOptionPane.showMessageDialog(frameInputProjeto, "Placa solar com os seguintes dados:/nModelo:" + placaSolar.getModelo() + "/nCapacidade: " + placaSolar.getCapacidade() + "/nPreço: " + placaSolar.getPreco() + "/nLista de fabricantes: " + placaSolar.getFabricante() + "/nFoi cadastrado com sucesso!");
+        		}
+        	}
+        });
+        
+        buttonSubmeterInversor.addActionListener(e -> {
+        	String modeloDoInversor = textFieldModeloDoInversor.getText();
+        	String potencia = textFieldPotencia.getText();
+        	String preçoDoInversor = textFieldPreçoDoInversor.getText();
+        	String fabricanteDoInversor = textFieldFabricanteDoInversor.getText();
+        	
+        	double doublePotencia = Double.valueOf(potencia);
+        	double doublePreçoDoInversor = Double.valueOf(preçoDoInversor);
+        	
+        	Fabricante objFabricanteDoInversor = null;
+        	
+        	for (Fabricante fabricante : fabricantes) {
+        		if (fabricante.getNome().equals(fabricanteDoInversor)) {
+        			objFabricanteDoInversor = fabricante;
+        		}
+        	}
+        	
+        	inversores.add(new Inversor(modeloDoInversor, doublePotencia, doublePreçoDoInversor, objFabricanteDoInversor));
+        	
+        	for (Inversor inversor : inversores) {
+        		if (inversor.getModelo().equals(modeloDoInversor)) {
+        			JOptionPane.showMessageDialog(frameInputInversor, "Inversor com os seguintes dados:/nModelo:" + inversor.getModelo() + "/nPotência: " + inversor.getPotencia() + "/nPreço: " + inversor.getPreco() + "/nFabricante: " + inversor.getFabricante() + "/nFoi cadastrado com sucesso!");
+        		}
+        	}
         });
 
         // Adiciona os botões para o panel
@@ -227,12 +342,50 @@ public class Main {
         
         panelInputProjeto.add(buttonSubmeterProjeto);
         
+        // Adiciona os labels e textfields para o panelInputFabricante
+        panelInputFabricante.add(labelNomeDoFabricante);
+        panelInputFabricante.add(textFieldNomeDoFabricante);
+        
+        panelInputFabricante.add(buttonSubmeterFabricante);
+        
+        // Adiciona os labels e textfields para o panelInputPlacaSolar
+        panelInputPlacaSolar.add(labelNomeDaPlacaSolar);
+        panelInputPlacaSolar.add(textFieldNomeDaPlacaSolar);
+        
+        panelInputPlacaSolar.add(labelCapacidade);
+        panelInputPlacaSolar.add(textFieldCapacidade);
+        
+        panelInputPlacaSolar.add(labelPreçoDaPlacaSolar);
+        panelInputPlacaSolar.add(textFieldPreçoDaPlacaSolar);
+        
+        panelInputPlacaSolar.add(labelFabricantes);
+        panelInputPlacaSolar.add(textFieldFabricantesDaPlacaSolar);
+        
+        panelInputPlacaSolar.add(buttonSubmeterPlacaSolar);
+        
+        // Adicionar os labels e textFields para o panelInputInversor
+        panelInputInversor.add(labelModeloDoInversor);
+        panelInputInversor.add(textFieldModeloDoInversor);
+        
+        panelInputInversor.add(labelPotencia);
+        panelInputInversor.add(textFieldPotencia);
+
+        panelInputInversor.add(labelPreçoDoInversor);
+        panelInputInversor.add(textFieldPreçoDoInversor);
+        
+        panelInputInversor.add(labelFabricanteDoInversor);
+        panelInputInversor.add(textFieldFabricanteDoInversor);
+        
+        panelInputInversor.add(buttonSubmeterInversor);
+        
         // Adiciona o panel para o frame
         frameInicial.add(panelInicial);
         frameCliente.add(panelCliente);
         frameFuncionario.add(panelFuncionario);
         frameInputCliente.add(panelInputCliente);
         frameInputProjeto.add(panelInputProjeto);
+        frameInputFabricante.add(panelInputFabricante);
+        frameInputPlacaSolar.add(panelInputPlacaSolar);
 
         // Centraliza o frame inicial
         frameInicial.setLocationRelativeTo(null);
